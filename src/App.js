ceb4e0bae327ac34e151db381b19dd86c72cf788
Jamille Silvestre <pjdeesilvestre@gmail.com>
2020-01-17 15:34:03 +0800

    design for login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAt, faKey } from '@fortawesome/free-solid-svg-icons'
+import heroImage from './assets/images/login-hero-image.jpeg';
+import logo from './assets/images/logo-white.png';
+import './assets/scss/main.scss';
+import './assets/scss/login.scss';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login">
+      <div
+        className="login-hero-image-container"
+        style={{
+          backgroundImage: `url(${heroImage})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover'
+        }}
+      >
+        <div className="hero-image-overlay">
+          <div className="login-heading">
+            <div className="login-main-header">
+              <img src={logo}/>
+            </div>
+            <div className="login-sub-header">A sample text to describe the site</div>
+          </div>
+        </div>
+      </div>
+      <div className="login-form-container">
+      <form className="login-form">
+          <div className="login-header">Welcome Back!</div>
+            <div>
+              <FontAwesomeIcon icon={faAt} className="input-icon" />
+              <input
+                text="email"
+                name="email"
+                placeholder="Email"
+                />
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faKey} className="input-icon" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                 />
+            </div>
+            <button className="submit-button" type="submit">Login</button>
+            <div className="forgot-password">Forgot password? Click here.</div>
+        </form>
+      </div>
     </div>
   );
 }
