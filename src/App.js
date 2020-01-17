15e7c066c542491c94661be659417d194d95975f
Jamille Silvestre <pjdeesilvestre@gmail.com>
2020-01-17 16:06:56 +0800

    added dropdown, sidenav, topnav

--- src/App.js
+++ src/App.js
@@ -1,56 +1,16 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAt, faKey } from '@fortawesome/free-solid-svg-icons'
-import heroImage from './assets/images/login-hero-image.jpeg';
-import logo from './assets/images/logo-white.png';
+import React, { Fragment } from 'react';
+import TopNav from './components/TopNav';
+import SideNav from './components/SideNav';
+import Login from './pages/Login';
 import './assets/scss/main.scss';
-import './assets/scss/login.scss';
-import './App.css';
 
 function App() {
   return (
-    <div className="login">
-      <div
-        className="login-hero-image-container"
-        style={{
-          backgroundImage: `url(${heroImage})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover'
-        }}
-      >
-        <div className="hero-image-overlay">
-          <div className="login-heading">
-            <div className="login-main-header">
-              <img src={logo}/>
-            </div>
-            <div className="login-sub-header">A sample text to describe the site</div>
-          </div>
-        </div>
-      </div>
-      <div className="login-form-container">
-      <form className="login-form">
-          <div className="login-header">Welcome Back!</div>
-            <div>
-              <FontAwesomeIcon icon={faAt} className="input-icon" />
-              <input
-                text="email"
-                name="email"
-                placeholder="Email"
-                />
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faKey} className="input-icon" />
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                 />
-            </div>
-            <button className="submit-button" type="submit">Login</button>
-            <div className="forgot-password">Forgot password? Click here.</div>
-        </form>
-      </div>
-    </div>
+    // <Login />
+    <Fragment>
+      <TopNav />
+      <SideNav />
+    </Fragment>
   );
 }
 
---
+++ src/components/TopNav.js
@@ -0,0 +1,54 @@
+import React, { useState, useContext } from 'react';
+import { faChevronRight, faUserCog, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import Dropdown from './Dropdown';
+import UserImage from '../assets/images/user-image.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {AuthContext} from '../context/Auth';
+// import { withRouter } from 'react-router-dom';
+import '../assets/scss/topnav.scss';
+
+function Topnav(props) {
+  // const { dispatch } = useContext(AuthContext);
+  // const logout = () => {
+  //   localStorage.removeItem('hris-user');
+  //   localStorage.removeItem('hris-is-authenticated');
+  //   dispatch({type: 'logout'});
+  //   props.history.push('/');
+  // }
+
+  return (
+    <div className="topnav">
+        <div className="breadcrumb-container">
+          <div className="breadcrumb">
+            <div className="breadcrumb-entry">Home
+              <FontAwesomeIcon icon={faChevronRight} className="breadcrumb-icon" />
+            </div>
+            <div className="breadcrumb-entry active">Dashboard</div>
+          </div>
+        </div>
+        <div className="user-nav">
+          <div className="user-details">
+            <img src={UserImage} className="user-image" />
+            <div className="user-name pointer-cursor">
+              <Dropdown
+               label="Starry Mae Gelvezon"
+               icon={faChevronDown}
+              >
+                <div>
+                  Settings
+                </div>
+                <div>
+                  Logout
+                </div>
+              </Dropdown>
+            </div>
+            <div className="user-settings">
+              <FontAwesomeIcon icon={faUserCog} className="settings-icon"/>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Topnav;
